505b823395131f2454dc008c31e4976eb3175823
Haramohan Sahu <hara.sahu@gmail.com>
2021-04-12 23:32:30 +0530

    steam example

---
+++ CollectionMain.java
@@ -0,0 +1,23 @@
+package com.collection;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.collectiomn.stream.StreamPractice;
+
+/**
+ * @author haramohan.sahu
+ *
+ */
+public class CollectionMain {
+	
+	
+
+	public static void main(String[] args) {
+	
+		List<String> myList = Arrays.asList("a1", "a2", "b1", "c2", "c1");	
+		List<Integer> list=Arrays.asList(11,22,3,4,5,7,14);
+System.out.println(StreamPractice.sumStream(list));
+	}
+
+}
