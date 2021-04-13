1e75489008cf4162c5dca3e3d9eee2b8df78b75e
sahu74 <hara.sahu@gmail.com>
2021-04-13 08:36:10 +0530

    adding stream other utility function

--- src/com/collection/CollectionMain.java
+++ src/com/collection/CollectionMain.java
@@ -2,6 +2,8 @@ package com.collection;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import com.collection.stream.StreamPractice;
 
@@ -10,14 +12,24 @@ import com.collection.stream.StreamPractice;
  *
  */
 public class CollectionMain {
-	
-	
+
+
 
 	public static void main(String[] args) {
-	
+/*
 		List<String> myList = Arrays.asList("a1", "a2", "b1", "c2", "c1");	
 		List<Integer> list=Arrays.asList(11,22,3,4,5,7,14);
-System.out.println(StreamPractice.sumStream(list));
+		System.out.println(StreamPractice.sumStream(list));
+		System.out.println("===========StreamfindFirst & ifPresent=====================");
+		StreamPractice.streamIfPresent();
+		System.out.println("===========streamOf=====================");
+		StreamPractice.streamOf();
+		System.out.println("===========IntStrea Example=====================");
+		StreamPractice.intStreaRange();
+		*/
+		Stream.of("a1A", "a2B", "a3C").map(s -> s.substring(0)).forEach(System.out::println   );
+		IntStream.range(1, 4).mapToObj( (i) -> "M" + i).forEach(System.out::println);
+
 	}
 
 }
--- src/com/collection/stream/StreamPractice.java
+++ src/com/collection/stream/StreamPractice.java
@@ -1,13 +1,50 @@
 package com.collection.stream;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+/*
+ * Stream operations are either intermediate or terminal. Intermediate operations return a stream so we can chain multiple 
+ * intermediate operations without using semicolons. Terminal operations are either void or return a non-stream result. 
+ * In the above example filter, map and sorted are intermediate operations whereas forEach is a terminal operation.
+ */
 
 public class StreamPractice {
 	public static Long sumStream(List<Integer> list)
 	{
-		
+
 		return list.stream().filter(i-> i > 10).mapToLong(i -> i).sum();
-		
+
+	}
+	/*
+	 * Just use Stream.of() to create a stream from a bunch of object references.
+	 */
+	public static void streamOf()
+	{
+		Stream.of("A1", "A2", "A3", "A4")
+		.findFirst()
+		.ifPresent(System.out::println);  // a1
+
+	}
+	/*
+	 * Calling the method stream() on a list of objects returns a regular object stream
+	 */
+	public static void streamIfPresent()
+	{
+		Arrays.asList("a1", "a2", "a3")
+		.stream().findFirst()
+		.ifPresent(System.out::println);  // a1
+	}
+	public static void streamFilter()
+	{
+		Arrays.asList("a1", "a2", "a3")
+		.stream().filter(i->i.startsWith("a")).forEach(System.out::println);
+	}
+
+	public static void intStreaRange()
+	{
+		IntStream.range(1, 4).forEach(System.out::println);
 	}
 
 }
